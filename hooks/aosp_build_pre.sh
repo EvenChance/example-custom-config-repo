028239242507017d15b642cd7f93afe6ae331b03
EvenChance <71563546+EvenChance@users.noreply.github.com>
2021-03-25 20:58:19 +0000

    Update aosp_build_pre.sh

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -18,6 +18,6 @@ patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/90001-increase-bin
 # apply custom hosts file
 custom_hosts_file="https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"
 echo "applying custom hosts file ${custom_hosts_file}"
-retry wget -q -O "${AOSP_BUILD_DIR}/system/core/rootdir/etc/hosts" "${custom_hosts_file}"
+#retry wget -q -O "${AOSP_BUILD_DIR}/system/core/rootdir/etc/hosts" "${custom_hosts_file}"
 echo '192.168.20.2  beijing.lan' >> "${AOSP_BUILD_DIR}/system/core/rootdir/etc/hosts"
 echo '192.168.20.11  htpc.lan' >> "${AOSP_BUILD_DIR}/system/core/rootdir/etc/hosts"
