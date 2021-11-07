4cf7589ce3d5900845a63ffd293898e44f4cf799
Sam <sam@sroberts.me>
2021-11-07 15:18:09 +0000

    Disable non-essential patches until upgraded

--- hooks/aosp_build_pre.sh
+++ hooks/aosp_build_pre.sh
@@ -8,10 +8,10 @@ community_patches_dir="${ROOT_DIR}/community_patches"
 rm -rf "${community_patches_dir}"
 git clone https://github.com/EvenChance/community_patches "${community_patches_dir}"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00001-global-internet-permission-toggle.patch"
-patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00002-round-icon.patch"
+#patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00002-round-icon.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00003-enable-volte-wifi-calling.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/00004-use-cloudflare-dns.patch"
-patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/90000-move-statusbar-clock-back-to-rhs.patch.patch"
+#patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/90000-move-statusbar-clock-back-to-rhs.patch.patch"
 patch -p1 --no-backup-if-mismatch < "${community_patches_dir}/90001-increase-binder-proxy-counting-watermarks.patch"
 
 
